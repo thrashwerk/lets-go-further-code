d4b6d1a1fbb679fcb13ac6bc5f8919cf061a8de4
Deividas Burškaitis <thrashwerk@mail25.net>
2025-03-13 13:56:42 +0200

    Ch05: Add a PSQL driver and make a connection pool

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // application version; later we'll generate it automatically
@@ -16,6 +20,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // struct to hold dependencies
@@ -29,9 +36,18 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pasas456@127.0.0.1/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -48,7 +64,28 @@ func main() {
 
 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// returns an sql.DB connection pool
+func openDB(cfg config) (*sql.DB, error) {
+	// create empty connection pool
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// establish a new connection to the DB
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
--- greenlight/go.mod
+++ greenlight/go.mod
@@ -3,3 +3,5 @@ module github.com/thrashwerk/lets-go-further-code/greenlight
 go 1.24.0
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.9
