1f9de0ad0dbd768ae36c57e553108ecc3d1890d1
Deividas Burškaitis <thrashwerk@mail25.net>
2025-03-18 18:11:10 +0200

    Ch07: Implement updating movie details

--- greenlight/internal/data/movies.go
+++ greenlight/internal/data/movies.go
@@ -85,8 +85,23 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	return &movie, nil
 }
 
+// the client must provide all movie details, can't update single field
 func (m MovieModel) Update(movie *Movie) error {
-	return nil // placeholder
+	query := `
+		UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error {
