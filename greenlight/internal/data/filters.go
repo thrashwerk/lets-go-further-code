f03d02f7bb434871b800f8e6aa8704a849fc4d6b
Deividas Burškaitis <thrashwerk@mail25.net>
2025-03-26 16:08:50 +0200

    Ch09: Add pagination metadata to movie list responses

--- greenlight/internal/data/filters.go
+++ greenlight/internal/data/filters.go
@@ -47,3 +47,25 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitzero"`
+	PageSize     int `json:"page_size,omitzero"`
+	FirstPage    int `json:"first_page,omitzero"`
+	LastPage     int `json:"last_page,omitzero"`
+	TotalRecords int `json:"total_records,omitzero"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
