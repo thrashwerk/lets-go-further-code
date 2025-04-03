9aaa5dfda9f4d049fbdbfbce96af10736fc67110
Deividas Burškaitis <thrashwerk@mail25.net>
2025-04-03 14:34:34 +0300

    Ch13: Use the helper function to send a welcome email to the user when they register

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -8,8 +8,10 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/lib/pq"
 	"github.com/thrashwerk/lets-go-further-code/greenlight/internal/data"
+	"github.com/thrashwerk/lets-go-further-code/greenlight/internal/mailer"
+
+	_ "github.com/lib/pq"
 )
 
 // application version; later we'll generate it automatically
@@ -30,6 +32,13 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 // struct to hold dependencies
@@ -37,6 +46,7 @@ type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer *mailer.Mailer
 }
 
 func main() {
@@ -44,6 +54,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 10, "PostgreSQL max idle connections")
@@ -51,6 +62,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2.0, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "f715d8465c1152", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "3c5b662640d188", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.mail25.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -64,10 +81,17 @@ func main() {
 	defer db.Close()
 	logger.Info("database connection pool established")
 
+	mailer, err := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer,
 	}
 
 	err = app.serve()
