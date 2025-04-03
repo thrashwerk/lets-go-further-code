7dcf9d84914aded6c8e34daf3bbe30056ba87ef5
Deividas Burškaitis <thrashwerk@mail25.net>
2025-04-03 14:37:16 +0300

    Ch13: Make the email sender retry 2 more times if it fails

--- greenlight/internal/mailer/mailer.go
+++ greenlight/internal/mailer/mailer.go
@@ -85,5 +85,16 @@ func (m *Mailer) Send(recipient string, templateFile string, data any) error {
 	msg.SetBodyString(mail.TypeTextPlain, plainBody.String())
 	msg.AddAlternativeString(mail.TypeTextHTML, htmlBody.String())
 
-	return m.client.DialAndSend(msg)
+	for i := 1; i <= 3; i++ {
+		err = m.client.DialAndSend(msg)
+		if err == nil {
+			return nil
+		}
+
+		if i != 3 {
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+
+	return err
 }
